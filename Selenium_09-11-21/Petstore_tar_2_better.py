e7ffa06ff337f219f002eb14a2d26f6fd75cbe35
droryair <drorya2324@gmail.com>
2021-11-09 17:29:32 +0200

    improving Petstore_tar_2_better.py

--- Selenium_09-11-21/Petstore_tar_2_better.py
+++ Selenium_09-11-21/Petstore_tar_2_better.py
@@ -22,18 +22,25 @@ driver.maximize_window()
 driver.implicitly_wait(5)
 driver.get("https://petstore.octoperf.com/actions/Catalog.action")
 
+
+def str_to_float(str: str):
+    if '$' in str:
+        return float(str.split('$')[1].replace(",", ""))
+    return float(str.replace(",", ""))
+
+
 # choosing a random animal from the home page sidebar list.
-animals = driver.find_elements(By.CSS_SELECTOR,"div#SidebarContent>a")
+animals = driver.find_elements(By.CSS_SELECTOR, "div#SidebarContent>a")
 rand_animal = choice(animals)
 rand_animal.click()
 
 # choosing a random animal type from the table
-animal_types = driver.find_elements(By.CSS_SELECTOR,"table>tbody>tr>td>a")
+animal_types = driver.find_elements(By.CSS_SELECTOR, "table>tbody>tr>td>a")
 rand_type = choice(animal_types)
 rand_type.click()
 
 # choosing a random animal from the table
-animal_items_btn = driver.find_elements(By.CSS_SELECTOR,"table>tbody>tr>td>a.Button")
+animal_items_btn = driver.find_elements(By.CSS_SELECTOR, "table>tbody>tr>td>a.Button")
 rand_item = choice(animal_items_btn)
 rand_item.click()
 
@@ -41,21 +48,12 @@ rand_item.click()
 cart_table = driver.find_element(By.CSS_SELECTOR, "table>tbody")
 cart_table_rows = cart_table.find_elements(By.CSS_SELECTOR, "tr")
 
-list_quantities = []
-for row in cart_table_rows[1:]:
-    cells = row.find_elements(By.CSS_SELECTOR, "td")
-    print("len",len(cells))
-    print(cells[4].get_attribute("value"))
-    list_quantities.append(cells[5].get_attribute("value"))
-
-
 # creating a list of quantities for future random choice.
-# list_quantities = []
-# for row in cart_table_rows[1:]:
-#     list_quantities.append(row.find_element(By.XPATH, "//td[5]/input"))
-
-
-
+list_quantities = []
+for row in cart_table_rows[1:len(cart_table_rows) - 1]:
+    cells = row.find_elements(By.CSS_SELECTOR, "td")
+    quantity = cells[4].find_element(By.TAG_NAME, "input")
+    list_quantities.append(quantity)
 
 # choosing a random quantity for one of the items
 rnd_quantity = random.randint(1, 10)  # choosing random quantity to apply
@@ -71,39 +69,37 @@ update_cart_btn.click()
 cart_table = driver.find_element(By.CSS_SELECTOR, "table>tbody")
 cart_table_rows = cart_table.find_elements(By.CSS_SELECTOR, "tr")
 
-
 # initializing variable for the sum of the expected sub total
 expected_sub_total = 0
-
-### !!!
-# for row in cart_table_rows[1:len(cart_table_rows)-1]:
-#     cells = row.find_elements(By.CSS_SELECTOR, "td")
-#     print("@@@",cells[7].text)
-#     # existing_cost =
-### !!!
-
-
-
 # checking total cost for each item in the table
-for row in cart_table_rows[1:len(cart_table_rows)-1]:
-    existing_cost = float(row.find_element(By.XPATH, "//td[7]").text.split('$')[1])  # Total Cost column
-    existing_quantity = float(row.find_element(By.XPATH, "//td[5]/input").get_attribute("value"))
-    existing_price = float(row.find_element(By.XPATH, "//td[6]").text.split('$')[1])
-    expected_cost = existing_price * existing_quantity   # List Price column * Quantity column
+for row in cart_table_rows[1:len(cart_table_rows) - 1]:
+    cells = row.find_elements(By.CSS_SELECTOR, "td")
+    # getting the values of 'Total Cost', 'Quantity' and 'List Price' columns
+    existing_cost = str_to_float(cells[6].text)
+    existing_price = str_to_float(cells[5].text)
+    existing_quantity = str_to_float(cells[4].find_element(By.TAG_NAME, "input").get_attribute("value"))
+
+    expected_cost = existing_price * existing_quantity  # List Price column * Quantity column = expected Total Cost
+    # Checking expected 'Total Cost' against existing.
     if expected_cost == existing_cost:
-        print(f"Price Passed for row number:{cart_table_rows.index(row)}")
-        print(f"Expected cost: {expected_cost}, Existing cost: {existing_cost} ")
+        print(
+            f"Price Passed for row number:{cart_table_rows.index(row)}. Expected: {expected_cost}, Existing: {existing_cost}",
+            end="\t")
         expected_sub_total += expected_cost
     else:
-        print(f"Price FAILED for row number:{cart_table_rows.index(row)}")
-        print(f"Expected cost: {expected_cost}, Existing cost: {existing_cost} ")
+        print(
+            f"Price FAILED for row number:{cart_table_rows.index(row)}. Expected: {expected_cost}, Existing: {existing_cost}",
+            end="\t")
+    print()
 
 # getting existing sub total
-sub_total = float(driver.find_element(By.CSS_SELECTOR, "td[colspan='7']").text.split('$')[1])
+existing_sub_total = float(driver.find_element(By.CSS_SELECTOR, "td[colspan='7']").text.split('$')[1].replace(",", ""))
 
 # checking sub total
-if expected_sub_total == sub_total:
-    print("Sub Total Passed")
+if expected_sub_total == existing_sub_total:
+    print(f"Sub Total Passed, expected: {expected_sub_total}, existing: {existing_sub_total}")
 else:
-    print(f"Sub Total failed. expected:{expected_sub_total} existing: {sub_total} ")
+    print(f"Sub Total FAILED. expected:{expected_sub_total} existing: {existing_sub_total} ")
 
+sleep(1)
+driver.close()
